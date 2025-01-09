e25d71b8c01b94b2b9a29166648b3fd3c00a09a7
Jordan Miller <Jordan.kay@gmail.com>
2025-01-09 09:14:08 -0700

    bump

--- examples/evr_vanitygen.py
+++ examples/evr_vanitygen.py
@@ -15,6 +15,7 @@
 # Note this is not particularly secure as the private keys may remain accessible in (virtual) memory
 
 import argparse
+import re
 import os
 import sys
 import time
@@ -28,14 +29,25 @@ parser.add_argument('search', metavar='search string',
                     type=str, help='Search string')
 parser.add_argument('--anywhere', action="store_true",
                     help="Search for string anywhere in address (default: leading characters only)")
+parser.add_argument('--endswith', action="store_true",
+                    help="Search for string anywhere in address (default: leading characters only)")
+parser.add_argument('--ignorecase', action="store_true",
+                    help="Search for string anywhere in address (default: leading characters only)")
 args = parser.parse_args()
 
 search_s = args.search
 anywhere = args.anywhere
+endswith = args.endswith
+ignorecase = args.ignorecase
 
-if not anywhere and search_s[0] != "R":
-    print("First character of search string must be 'R'")
+if search_s[0] != "E" and not (anywhere or endswith):
+    print("First character of search string must be 'E'")
     sys.exit(1)
+
+if ignorecase:
+    case = re.IGNORECASE
+else:
+    case = 0
 
 try:
     evrmore.base58.decode(search_s)
@@ -47,21 +59,48 @@ evrmore.SelectParams('mainnet')
 
 c = 0
 start = time.time()
-while True:
-    privkey = CEvrmoreSecret.from_secret_bytes(os.urandom(32))
-    addr = str(P2PKHEvrmoreAddress.from_pubkey(privkey.pub))
-    c += 1
-    if anywhere:
-        if search_s in addr:
+
+if endswith:
+    pattern = search_s + r'$'
+    while True:
+        entropy = os.urandom(32)
+        privkey = CEvrmoreSecret.from_secret_bytes(entropy)
+        addr = str(P2PKHEvrmoreAddress.from_pubkey(privkey.pub))
+        if re.search(pattern, addr, case):
             break
-    else:
-        if addr.startswith(search_s):
+        c += 1
+        if c % 10000 == 0:
+            sec = time.time()-start
+            sys.stdout.write(f'Try {c} ({round(c/sec)} keys/s) ({(time.time()-start) / 60 / 60} hours)      \r')
+            sys.stdout.flush()
+elif anywhere:
+    pattern = search_s
+    while True:
+        entropy = os.urandom(32)
+        privkey = CEvrmoreSecret.from_secret_bytes(entropy)
+        addr = str(P2PKHEvrmoreAddress.from_pubkey(privkey.pub))
+        if re.search(pattern, addr, case):
             break
+        c += 1
+        if c % 10000 == 0:
+            sec = time.time()-start
+            sys.stdout.write(f'Try {c} ({round(c/sec)} keys/s) ({(time.time()-start) / 60 / 60} hours)      \r')
+            sys.stdout.flush()
+else:# startswith
+    pattern = r'^' + search_s
+    while True:
+        entropy = os.urandom(32)
+        privkey = CEvrmoreSecret.from_secret_bytes(entropy)
+        addr = str(P2PKHEvrmoreAddress.from_pubkey(privkey.pub))
+        if re.search(pattern, addr, case):
+            break
+        c += 1
+        if c % 10000 == 0:
+            sec = time.time()-start
+            sys.stdout.write(f'Try {c} ({round(c/sec)} keys/s) ({(time.time()-start) / 60 / 60} hours)      \r')
+            sys.stdout.flush()
 
-    if c % 1000 == 0:
-        sec = time.time()-start
-        sys.stdout.write(f'Try {c} ({round(c/sec)} keys/s)\r')
-        sys.stdout.flush()
-
-
-print(addr, privkey)
+print()
+print((time.time()-start) / 60 / 60, 'hours')
+print(addr, privkey, entropy.hex())
+print(entropy)
